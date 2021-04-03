dbb40d0ebccd015971ba8ae8cee52aa3da8d6eac
Hypertoken <austin.g.pereira@gmail.com>
2021-04-03 15:21:06 -0700

    Bontech is back
    Added the BMG in direct configuration.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -25, 12, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 36, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1127,7 +1127,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -38
+#define X_MIN_POS -17
 #define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE +8
